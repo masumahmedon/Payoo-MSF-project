3b91f9ebc1bed1bf24dcfd0667846367a5da06f4
masumahmedon <masumahmed.ict@gmail.com>
2024-09-25 00:43:21 +0600

    add money & pin number

--- js/home.js
+++ js/home.js
@@ -6,4 +6,23 @@ document.getElementById('btn-add-money')
     const addMoneyInput = document.getElementById('add-money-input').value;
     const pinNumberInput = document.getElementById('pin-number-input').value;
     console.log(addMoneyInput, pinNumberInput);
+
+    if(pinNumberInput === '1234'){
+        console.log('adding money to your account')
+
+        const balance = document.getElementById('account-balance')
+        .innerText;
+        console.log(balance)
+
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance)
+        
+        document.getElementById('account-balance').innerText = newBalance;
+        
+    }
+    else{
+        alert('Please Try Again.')
+    }
 });
